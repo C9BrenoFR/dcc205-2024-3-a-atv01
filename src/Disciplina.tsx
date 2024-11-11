ece387de46462972304e02104aa81226d9ece8e5
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-11-11 20:40:55 -0300

    Extrai constantes

--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,8 +1,12 @@
+import "./Disciplina.css"
+
 export default function Disciplina() {
+    const codigo = "DCC207";
+    const nomeDisplicina = "Desenvolvimento Web Front-End";
   return (
-    <div>
-      <h2>DCC207</h2>
-      <p>Desenvolvimento Web Front-End</p>
+    <div className="disciplina">
+      <div>{codigo}</div>
+      <div>{nomeDisplicina}</div>
     </div>
   )
 }
