901839e936d52e791d8323d005f3bad5d67f4792
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-11-11 19:44:45 -0300

    Altera base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dcc207-2024-3-a-atv01',
+  base: "/ufjf-dcc207-2024-3-a-atv01",
   plugins: [react()],
 })
